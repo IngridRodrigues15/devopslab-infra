21c8c7861294f15815a7db734f75b9dade7ad4b9
Ingrid Rodrigues <ingrid.s.rodrigues@hotmail.com>
2021-08-14 11:40:52 -0300

    change google credentials to env var

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/vagrant/labdevopscloud-ingrid-24f256698030.json")
+  
 
   project = "labdevopscloud-ingrid"
   region  = "us-central1"
